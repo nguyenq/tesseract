3ca119d48fb71508de601978d67d933ea660a6fb
nguyenq <nguyenq@users.sf.net>
2023-07-28 22:43:06 -0500

    Upgrade to Tesseract 5.3.2

--- src/Tesseract.Tests/EngineTests.cs
+++ src/Tesseract.Tests/EngineTests.cs
@@ -20,7 +20,7 @@ namespace Tesseract.Tests
         {
             using (var engine = CreateEngine())
             {
-                Assert.That(engine.Version, Does.StartWith("5.3.1"));
+                Assert.That(engine.Version, Does.StartWith("5.3.2"));
             }
         }
 
