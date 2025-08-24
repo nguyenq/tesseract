705439025d0468e450ac71c685f5ad3647561a0a
nguyenq <nguyenq@users.sf.net>
2025-08-23 23:43:28 -0500

    Fix System.ArgumentNullException

--- src/Tesseract/Internal/InteropDotNet/LibraryLoader.cs
+++ src/Tesseract/Internal/InteropDotNet/LibraryLoader.cs
@@ -78,6 +78,8 @@ namespace InteropDotNet
         {
             var baseDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             Logger.TraceInformation("Checking executing application domain location '{0}' for '{1}' on platform {2}.", baseDirectory, fileName, platformName);
+            if (string.IsNullOrEmpty(baseDirectory))
+                return IntPtr.Zero;
             return InternalLoadLibrary(baseDirectory, platformName, fileName);
         }
 
